ffc022d38472a45a8a4ea8918f379d9f6beeb626
Gaby Clarke <gabyclarke@me.com>
2017-04-02 22:59:24 +0200

    working location to day/night

--- OuterSpace/sun/js/script.js
+++ OuterSpace/sun/js/script.js
@@ -1,9 +1,10 @@
 
+// Documentation: http://sunrise-sunset.org/api
+
 $(document).ready(function(){
 
-	// Documentation: http://sunrise-sunset.org/api
-	var lat = 48.864716;
-	var long = 2.349014;
+	var lat;
+	var long;
 	var rise;
 	var set;
 	var d = new Date();
@@ -12,30 +13,48 @@ $(document).ready(function(){
 	var s = d.getSeconds();
 	var now;
 
-	$.ajax({
-		type: 'GET',
-		url: "http://api.sunrise-sunset.org/json?lat="+lat+"&lng="+long+"&date=today&formatted=0",
-		dataType: 'json',
-		success: function (data) {
-			rise = data.results.sunrise.slice(11,19);
-			set = data.results.sunset.slice(11,19);
-			now = h+":"+m+":"+s;
+	var output = document.getElementById("out");
 
-			if (now > rise ){
-			document.body.style.background = "#F4B908";
-			}
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser.</p>";
+    return;
+  }
 
+  function success(position) {
+    lat = position.coords.latitude.toFixed(8);
+    long = position.coords.longitude.toFixed(8);
+    // output.innerHTML = '<p>Latitude is ' + lat + '° <br>Longitude is ' + long + '°</p>';
+		$.ajax({
+			type: 'GET',
+			url: "http://api.sunrise-sunset.org/json?lat="+lat+"&lng="+long+"&date=today&formatted=0",
+			dataType: 'json',
+			success: function (data) {
+				rise = data.results.sunrise.slice(11,19);
+				console.log(rise);
+				set = data.results.sunset.slice(11,19);
+				now = h+":"+m+":"+s;
 
-			if (now>set) {
-				document.body.style.background = "#051F2B";
-			}
+				if (now > rise && now < set){
+					document.body.style.background = "#F4B908";
+				} else {
+					document.body.style.background = "#051F2B";
+				}
+				//
+				// $(".data").append('<p>'+rise+'</p>');
+				// $(".data").append('<p>'+set+'</p>');
+				// $(".data").append('<p>'+h+":"+m+":"+s+'</p>');
 
-			$(".data").append('<p>'+rise+'</p>');
-			$(".data").append('<p>'+set+'</p>');
-			$(".data").append('<p>'+h+":"+m+":"+s+'</p>');
+			},
+			error: function() { alert('Failed!'); },
+		});
+  }
 
-			// console.log(data.results.sunset)
-		},
-		error: function() { alert('Failed!'); },
-	});
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  }
+
+  // output.innerHTML = "<p>Locating…</p>";
+
+	navigator.geolocation.getCurrentPosition(success, error);
+
 });
