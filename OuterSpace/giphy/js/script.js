9c73dc02371713f8b434ee993c05648a9b13287b
Gaby Clarke <gabyclarke@me.com>
2017-04-03 10:54:59 +0200

    working 3-column, beginning mobile response

---
+++ OuterSpace/giphy/js/script.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+
+	// For documentation check : https://github.com/Giphy/GiphyAPI
+	var apiKey = "dc6zaTOxFJmzC";
+	var request = "rad";
+	var limit = 12;
+
+	$.ajax({
+		type: 'GET',
+		url: "http://api.giphy.com/v1/gifs/search?q="
+				 +request+"&api_key="+apiKey+"&limit="+limit,
+		dataType: 'json',
+		success: function (data) {
+			console.log(data.data[0]);
+			for(var i = 0; i<limit; i++){
+				if(data.data[i]){
+					$(".data").append('<li><img src="'
+										+data.data[i].images.downsized_large.url+'"></li>');
+				}
+			}
+
+		},
+		error: function() { alert('Failed!'); },
+	});
+});
