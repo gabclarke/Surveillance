0627e861291de1dd6e884119824388c2d5c1d92b
Gaby Clarke <gabyclarke@me.com>
2017-04-19 16:15:02 +0200

    attempt to set up glitch.js and html2canvas

---
+++ OuterSpace/glitchy/js/script.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function(){
+
+	// For documentation check : https://github.com/Giphy/GiphyAPI
+	var apiKey = "dc6zaTOxFJmzC";
+	var request = "rad";
+	var limit = 12;
+
+	$.ajax({
+		type: 'GET',
+		url: "http://api.giphy.com/v1/gifs/search?q="
+				 +request+"&api_key="+apiKey+"&limit="+limit,
+		dataType: 'json',
+		success: function (data) {
+			console.log(data.data[0]);
+			for(var i = 0; i<limit; i++){
+				if(data.data[i]){
+					$(".data").append('<li class="col col-m-6 col-4"><img src="'
+										+data.data[i].images.downsized_large.url+'"></li>');
+				}
+			}
+
+		},
+		error: function() { alert('Failed!'); },
+	});
+
+	glitch(document.getElementById("glitched"), {
+    amount: 8,
+    complete: function(canvas){
+        // do something with the glitched canvas
+    }
+});
+
+});
