2a4a1b369510aae394ea42d1d78956ee63a77e8f
Gaby Clarke <gabyclarke@me.com>
2017-04-30 23:07:14 +0200

    working news roll

--- OuterSpace/news/js/script.js
+++ OuterSpace/news/js/script.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function(){
 
+// API call format:
 // http://api.nytimes.com/svc/news/{version}/content/{source}/{section}[/time-period][.response-format]?api-key={your-API-key}
-// http://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=bb61482c14134196b907cea6abe3e8e2
 
 	var url = "https://api.nytimes.com/svc/news/v3/content/all/all.json";
-	url += '?api-key=bb61482c14134196b907cea6abe3e8e2';
+	url += "?YOURAPI";
 	console.log('attempting');
 
 	$.ajax({
@@ -16,7 +16,11 @@ $(document).ready(function(){
 			console.log(data.results);
 			for (var i = 0; i<30; i++){
 				if(data.results[i]){
-					$(".data").append("<li>" + data.results[i].title + "</li>")
+					$(".data").append("<li>")
+					$(".data").append("<h1>" + data.results[i].title + "</h1>")
+					$(".data").append("<p>" + data.results[i].byline + "<p>")
+					$(".data").append("<p>" + data.results[i].abstract + "<p>")
+					$(".data").append("</li>")
 				}
 			}
 		}
@@ -29,32 +33,5 @@ $(document).ready(function(){
 	//
 	// }).fail(function(err) {
 	//   throw err;
-	// });
-
-	// var apiKey = "bb61482c14134196b907cea6abe3e8e2";
-	//
-	// $.ajax({
-	// 	type: 'GET',
-	// 	// url: "http://api.giphy.com/v1/gifs/search?q="
-	// 	// 		 +request+"&api_key="+apiKey+"&limit="+limit,
-	// 	url: "http://api.nytimes.com/svc/news/v3/content/all/all?limit=15&api-key=" + apiKey,
-	// 	dataType: 'json',
-	// // 	success: function (data) {
-	// // 		console.log(data)
-	// // 		// console.log(data.data[0]);
-	// // 		// for(var i = 0; i<limit; i++){
-	// // 		// 	if(data.data[i]){
-	// // 		// 		$(".data").append('<li class="col col-m-6 col-4"><img src="'
-	// // 		// 							+data.data[i].images.downsized_large.url+'"></li>');
-	// // 		// 	}
-	// // 		// }
-	// //
-	// // 	},
-	// // 	error: function() { alert('Failed!'); },
-	// // });
-	// }).done(function(result) {
-	// console.log(result);
-	// }).fail(function(err) {
-	// throw err;
 	// });
 });
