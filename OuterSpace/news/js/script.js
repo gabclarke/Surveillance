245ba48d5508739f0240c9e40daa4c35dbdce472
Gaby Clarke <gabyclarke@me.com>
2017-05-09 10:17:23 +0200

    working newsfeed, no images

--- OuterSpace/news/js/script.js
+++ OuterSpace/news/js/script.js
@@ -4,34 +4,29 @@ $(document).ready(function(){
 // API call format:
 // http://api.nytimes.com/svc/news/{version}/content/{source}/{section}[/time-period][.response-format]?api-key={your-API-key}
 
+	var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+	var today = new Date();
+	document.getElementById("today").innerHTML = today.toLocaleDateString("en-US",options);
+
 	var url = "https://api.nytimes.com/svc/news/v3/content/all/all.json";
-	url += "?YOURAPI";
-	console.log('attempting');
+	url += '?api-key=bb61482c14134196b907cea6abe3e8e2';
 
 	$.ajax({
 	  url: url,
 	  method: 'GET',
 		success: function (data) {
-			console.log('success');
 			console.log(data.results);
 			for (var i = 0; i<30; i++){
 				if(data.results[i]){
 					$(".data").append("<li>")
-					$(".data").append("<h1>" + data.results[i].title + "</h1>")
-					$(".data").append("<p>" + data.results[i].byline + "<p>")
+					// $(".data").append("<img src='" + data.results[i].thumbnail_standard + "'>")
+					$(".data").append("<h1><a href='" + data.results[i].url + "'>" + data.results[i].title + "</a></h1>")
+					$(".data").append("<p class='byline'>" + data.results[i].byline + "<p>")
 					$(".data").append("<p>" + data.results[i].abstract + "<p>")
 					$(".data").append("</li>")
 				}
 			}
+			$(".data").append("<h3 id='end'> That's the newest news. </h3>")
 		}
 	})
-	// }).done(function(result) {
-	//   console.log(result);
-	// 	for (var i = 0; i++){ // might want to limit this
-	//
-	// 	}
-	//
-	// }).fail(function(err) {
-	//   throw err;
-	// });
 });
