3d062f061f1a7ef878e1f5e113745fc107e16992
Gaby Clarke <gabyclarke@me.com>
2017-05-17 10:31:00 +0200

    working, updated documentation

--- OuterSpace/news/js/header.js
+++ OuterSpace/news/js/header.js
@@ -1,3 +1,7 @@
+// The Newest News
+// Gaby Clarke, Spring 2017
+// Parsons Paris, Spatial Lab
+
 // Adapted from code at http://codepen.io/Dannzzor/pen/dlAap
 
 $(document).ready(function(){
--- OuterSpace/news/js/script.js
+++ OuterSpace/news/js/script.js
@@ -1,3 +1,7 @@
+// The Newest News
+// Gaby Clarke, Spring 2017
+// Parsons Paris, Spatial Lab
+
 $(document).ready(function(){
 
 // API call format:
@@ -21,13 +25,14 @@ $(document).ready(function(){
 					$(".data").append("<li>")
 					$(".data").append("<h1 class='change' data-text='" + data.results[i].title + "'><a href='" + data.results[i].url + "'>" + data.results[i].title + "</a></h1>")
 					$(".data").append("<p class='byline'>" + data.results[i].byline + "<p>")
-					$(".data").append("<p class='change' data-text='" + data.results[i].abstract + "'>" + data.results[i].abstract + "<p>")
+					$(".data").append("<p class='change2' data-text='" + data.results[i].abstract + "'>" + data.results[i].abstract + "<p>")
 					$(".data").append("</li>")
 				}
 			}
 			$(".data").append("<h3 id='end' class='flip'> That's the newest news. Updated " + today.toLocaleTimeString() + ".</h3>")
 		}
 	});
+
 
 });
 
@@ -36,14 +41,18 @@ function printGlitch() {
 	document.getElementsByClassName("logo")[0].setAttribute("class", "logo glitch");
 	document.getElementsByClassName("flip")[0].setAttribute("class", "upsidedown");
 	var all = document.getElementsByClassName("change");
+	var all2 = document.getElementsByClassName("change2");
 	console.log(all);
 	console.log(all.length);
-	var i;
-	for (i = 0; i < all.length; i++) {
-		console.log(all[i]);
+	for (var i = 0; i < all.length; i++) {
 		var rand = Math.floor((Math.random() * 14));
 		var rand2 = Math.floor((Math.random() * 14));
 	  all[i].setAttribute("class", "glitch" + rand + " glitch" + rand2);
+	}
+	for (var i = 0; i < all2.length; i++) {
+		var rand = Math.floor((Math.random() * 14));
+		var rand2 = Math.floor((Math.random() * 14));
+	  all2[i].setAttribute("class", "glitch" + rand + " glitch" + rand2);
 	}
 	window.print();
 
